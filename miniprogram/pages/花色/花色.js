d10e6ebdcf9fc0c6351c8e9590dee335cc94c065
yuanlq <yuanlq@pku.edu.cn>
2023-04-11 19:46:25 +0800

    可以通过填写内容修改数据库

--- miniprogram/pages/花色/花色.js
+++ miniprogram/pages/花色/花色.js
@@ -2,9 +2,7 @@ const app = getApp();
 var classification = 1;
 Page({
   data: {
-
     cat: [],
-
     screenWidth: 0,
     screenHeight: 0,
     imgwidth: 0,
@@ -22,26 +20,22 @@ Page({
     this.loadMoreCat();
   },
 
-
   loadMoreCat() {
-
     const cat = this.data.cat;
-    app.mpServerless.db.collection('WanliuMeow').find(
-      {
-        classification: classification,
-        status: "健康"
-      },
-      {
-        // sort: { pinyin: 1 },
-        skip: cat.length,
-        limit: 20,
-      }
-    ).then(res => {
-      const { result: data } = res;
-      this.setData({ cat: cat.concat(data) });
+    app.mpServerless.db.collection('WanliuMeow').find({
+      classification: classification,
+      status: "健康"
+    }, {
+      // sort: { pinyin: 1 },
+      skip: cat.length,
+      limit: 20,
+    }).then(res => {
+      const {
+        result: data
+      } = res;
+      this.setData({
+        cat: cat.concat(data)
+      });
     }).catch(console.error);
-
   },
-
 })
-
