9cd9be3cac988f6d74aa31c138a4537005f74867
yuanlq <yuanlq@pku.edu.cn>
2023-05-01 12:24:41 +0800

    增加json转Excel的代码

--- manage/json2Excel.py
+++ manage/json2Excel.py
@@ -1,29 +1,44 @@
 # !/usr/bin/python
 #  -*- coding: UTF-8 -*-
 
+import pandas as pd
 import datetime
 import json
-import argparse
 import sys
 import os
 import time
-import urllib.parse
-import urllib
-import openpyxl
-import json
 
 today = time.strftime("%Y-%m-%d", time.localtime())
 
 print(today)
 
 new_list = []
-with open("all.json", encoding='utf-8') as f:
+with open("input.json", encoding='utf-8') as f:
     lines = f.readlines()
-    alljson = ' '.join(lines).replace(' ', '').replace('\n', '')
-    z = filter(lambda x: x, alljson.split("}"))
-    for i in z:
-        ii = i+'}'
-        new_list.append(ii)
-        y = json.loads(ii)
-        print(y)
-    # print(new_list)
+    lines = [x.strip() for x in lines if x.strip() != '']
+    for line in lines:
+        y = json.loads(line)
+        new_list.append(y)
+
+# 改变表格列的顺序
+df = pd.DataFrame(new_list)
+col = list(df.columns)
+col.remove('name')
+col.remove('_id')
+col.insert(0, 'name')
+col.insert(0, '_id')
+df = df[col]
+# df['_id'] = df['_id']['$oid']
+for row in range(df.shape[0]):
+    df['_id'][row] = df['_id'][row]['$oid']
+
+df.sort_values('_id')
+
+if not os.path.exists('表格备份'):
+    os.mkdir('表格备份')
+
+# 写入 csv 表格
+df.to_csv("表格备份/" + today + ".csv", index_label="index_label")
+
+# 写入 Excel 表格
+df.to_excel("表格备份/" + today + ".xlsx")
